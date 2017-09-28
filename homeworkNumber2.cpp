ba10f0fb6d186f4673d23715d98731fc620b4b42
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-09-28 21:41:03 +0300

    Update homeworkNumber2.cpp

--- homeworkNumber2.cpp
+++ homeworkNumber2.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include <math.h>
 
-int main() {    
+int main() {   
     int n;
     std::cout << "Введите число" << std::endl;
     std::cin >> n;
-    n = fmod(n,100) * 10 + n / 100;
+    n = fmod(n, 100) * 10 + n / 100;
     std::cout << n << std::endl;
     return 0;
 }
